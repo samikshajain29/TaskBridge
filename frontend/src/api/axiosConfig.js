ebe09ed6ba8fbcb3b5430a5d6711ac7f24812749
Samiksha Jain <samikshaj7371@gmail.com>
2026-05-05 16:06:23 +0530

    Update axiosConfig.js

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://taskbridgeserver.onrender.com",
   withCredentials: true, // Necessary to send/receive httpOnly cookies for refresh token
 });
 
